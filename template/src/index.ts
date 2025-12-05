ffc6c5fa563a27539eff34844941aff2b9f0a29c
Xaho <mauriceampt@hotmail.com>
2025-12-05 22:13:35 +0100

    update template

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,3 +1,6 @@
 import * as fs from 'fs';
+import {process} from './process.js';
+import * as assert from 'node:assert';
 
 const input = fs.readFileSync('input.txt', 'utf8');
+assert.equal(process(input), 0);
---
+++ template/src/process.ts
@@ -0,0 +1,3 @@
+export function process(input: string) {
+    return 0;
+}
